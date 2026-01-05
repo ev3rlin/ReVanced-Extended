008847a74bbc923f7cbeea8ca46a40db2ce93d22
Denis Slavenko <slavenkodenis@gmail.com>
2026-01-05 07:56:01 +0300

    fix: resolve merge conflicts

--- build.sh
+++ build.sh
@@ -134,11 +134,7 @@ for table_name in $(toml_get_table_names); do
 	fi
 
 	app_args[include_stock]=$(toml_get "$t" include-stock) || app_args[include_stock]=true && vtf "${app_args[include_stock]}" "include-stock"
-<<<<<<< HEAD
-	app_args[dpi]=$(toml_get "$t" dpi) || app_args[dpi]="nodpi"
-=======
 	app_args[dpi]=$(toml_get "$t" dpi) || app_args[dpi]="$DEF_DPI_LIST"
->>>>>>> bc3646b5b31a6774539d22f72f4d45ad31a339a3
 	table_name_f=${table_name,,}
 	table_name_f=${table_name_f// /-}
 	app_args[module_prop_name]=$(toml_get "$t" module-prop-name) || app_args[module_prop_name]="${table_name_f}-jhc"
@@ -173,15 +169,13 @@ wait
 rm -rf temp/tmp.*
 if [ -z "$(ls -A1 "${BUILD_DIR}")" ]; then abort "All builds failed."; fi
 
-<<<<<<< HEAD
 # Initial changelog logic
+# log "\nInstall [Microg](https://github.com/ReVanced/GmsCore/releases) for non-root YouTube and YT Music APKs"
+# log "Use [zygisk-detach](https://github.com/j-hc/zygisk-detach) to detach root ReVanced YouTube and YT Music from Play Store"
+# log "\n[revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)\n"
+# log "$(cat "$TEMP_DIR"/*-rv/changelog.md)"
+
 log "\n$(cat "$TEMP_DIR"/*-rv/changelog.md)"
-=======
-log "\nInstall [Microg](https://github.com/ReVanced/GmsCore/releases) for non-root YouTube and YT Music APKs"
-log "Use [zygisk-detach](https://github.com/j-hc/zygisk-detach) to detach root ReVanced YouTube and YT Music from Play Store"
-log "\n[revanced-magisk-module](https://github.com/j-hc/revanced-magisk-module)\n"
-log "$(cat "$TEMP_DIR"/*-rv/changelog.md)"
->>>>>>> bc3646b5b31a6774539d22f72f4d45ad31a339a3
 
 SKIPPED=$(cat "$TEMP_DIR"/skipped 2>/dev/null || :)
 if [ -n "$SKIPPED" ]; then
@@ -189,7 +183,6 @@ if [ -n "$SKIPPED" ]; then
 	log "$SKIPPED"
 fi
 
-<<<<<<< HEAD
 # New skipped changelog logic with links
 
 # SKIPPED=$(cat "$TEMP_DIR"/skipped 2>/dev/null || :)
@@ -206,6 +199,4 @@ fi
 # 	log "$(cat "$TEMP_DIR"/*-rv/changelog.md)"
 # fi
 
-=======
->>>>>>> bc3646b5b31a6774539d22f72f4d45ad31a339a3
 pr "Done"
